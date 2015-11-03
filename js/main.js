3424776d3355ea80309f78ca16f7036478cab495
Kenny Mark <markcoffiekenneth@gmaill.com>
2015-11-03 13:44:07 +0000

    retrying new design

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var button = document.getElementById.('send')onclick = function() {send()};
+
+function send {
+	button + window.location("thanks.html");
+
+};
+
+
+
+function myFunc() {
+    document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
