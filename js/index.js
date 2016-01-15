b1a57873d1ecde593d26b745fd058bf405e5cb9b
Kenny Mark <markcoffiekenneth@gmail.com>
2016-01-15 19:25:10 +0000

    removed blocking slider code

--- js/index.js
+++ js/index.js
@@ -88,40 +88,40 @@ $('ul a').click(function(e){
 });
 
 
-$(function() {
+// $(function() {
 
-    //settings for slider
-    var width = 720;
-    var animationSpeed = 1000;
-    var pause = 3000;
-    var currentSlide = 1;
+//     //settings for slider
+//     var width = 720;
+//     var animationSpeed = 1000;
+//     var pause = 3000;
+//     var currentSlide = 1;
 
-    //cache DOM elements
-    var $slider = $('.slider');
-    var $slideContainer = $('.slides', $slider);
-    var $slides = $('.slide', $slider);
+//     //cache DOM elements
+//     var $slider = $('.slider');
+//     var $slideContainer = $('.slides', $slider);
+//     var $slides = $('.slide', $slider);
 
-    var interval;
+//     var interval;
 
-    function startSlider() {
-        interval = setInterval(function() {
-            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-                if (++currentSlide === $slides.length) {
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                }
-            });
-        }, pause);
-    }
-    function pauseSlider() {
-        clearInterval(interval);
-    }
+//     function startSlider() {
+//         interval = setInterval(function() {
+//             $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+//                 if (++currentSlide === $slides.length) {
+//                     currentSlide = 1;
+//                     $slideContainer.css('margin-left', 0);
+//                 }
+//             });
+//         }, pause);
+//     }
+//     function pauseSlider() {
+//         clearInterval(interval);
+//     }
 
-    $slideContainer
-        .on('mouseenter', pauseSlider)
-        .on('mouseleave', startSlider);
+//     $slideContainer
+//         .on('mouseenter', pauseSlider)
+//         .on('mouseleave', startSlider);
 
-    startSlider();
+//     startSlider();
 
 
-});
+// });
